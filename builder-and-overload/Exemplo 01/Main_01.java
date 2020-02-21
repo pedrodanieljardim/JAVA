070882b49afe61a1cdd1b97a1b35fc5c0e6bfddd
pedrodanieljardim <51025466+pedrodanieljardim@users.noreply.github.com>
2020-02-21 12:25:33 -0300

    paradigm of builder and overload in o.o

---
+++ builder-and-overload/Exemplo 01/Main_01.java
@@ -0,0 +1,47 @@
+package Application;
+
+import entities.produto;
+import java.util.Scanner;
+
+public class program {
+	public static void main(String[] args) {
+				
+				Scanner sc= new Scanner(System.in);
+				
+				System.out.println("Cadastro de Produto \n");
+				System.out.println("digite o nome do produto:");
+				String nome=sc.nextLine();
+				System.out.println("digite o preço do produto:");
+				double preco=sc.nextDouble();
+				System.out.println("digite a quantidade do produto");
+				double quantidade=sc.nextDouble();
+				
+				// para usar o construtor, é necessário criar variáveis temporárias para servir de parâmetro para o construtor
+				// e instânciação do produto ocorre após a declaração das variáveis tempórarias!
+	
+				produto p1=new produto(nome,preco,quantidade);
+				
+				p1.calcula_estoque();
+				
+				System.out.println(p1);
+				
+				System.out.println("\n OPERADOR DE ENTRADA NO ESTOQUE\n");
+				double entrada;
+				System.out.println("\n Qual a quantidade a ser colocada no estoque?");
+				entrada=sc.nextDouble();
+				p1.entrada_produto(entrada);
+				
+				System.out.println(p1);
+				
+				System.out.println("\n OPERADOR DE SAIDA NO ESTOQUE \n");
+				double saida;
+				System.out.println("\n Qual a quantidade a ser retirada no estoque?");
+				saida=sc.nextDouble();
+				p1.saida_produto(saida);
+				
+				System.out.println(p1);
+				
+				sc.close();
+			}
+		}
+
